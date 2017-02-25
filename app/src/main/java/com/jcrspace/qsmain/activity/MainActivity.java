fc1713b7cca8b6fb884ac57f5d8ddcff7e374083
csgajcr <610691723@qq.com>
2017-02-25 10:30:17 +0800

    add module

--- app/src/main/java/com/jcrspace/qsmain/activity/MainActivity.java
+++ app/src/main/java/com/jcrspace/qsmain/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.jcrspace.qsmain;
+package com.jcrspace.qsmain.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.jcrspace.qsmain.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
